a8f5341392bcd1d056ae40214cab7c97b08f35c2
dennerricardo <denner.ricardo@gmail.com>
2017-10-21 14:20:18 -0200

    criado provider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Provider } from '../../providers/provider/provider';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [Provider]
 })
 export class HomePage {
 
